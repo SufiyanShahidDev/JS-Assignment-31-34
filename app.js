d6fad4c912f54136020c6b81197d97adfe7e9ff5
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2025-12-17 23:13:50 +0500

    Initial commit

--- app.js
+++ app.js
@@ -12,11 +12,38 @@ var now = new Date();
 var monthIndex = now.getMonth();
 
 var months = [
-  "January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
 ];
 
 var currentMonth = months[monthIndex];
 
 console.log("Current Month: " + currentMonth);
 alert("Current Month: " + currentMonth);
+
+// Answer3
+
+var now = new Date();
+var dayIndex = now.getDay();
+
+var days = [
+    "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
+];
+
+var shortDay = days[dayIndex];
+
+console.log("Today is: " + shortDay);
+alert("Today is: " + shortDay);
+
+// Answer4
+
+var now = new Date();
+var dayIndex = now.getDay();
+
+if (dayIndex === 0 || dayIndex === 6) {
+    alert("It's Fun day");
+    console.log("It's Fun day");
+} else {
+    alert("It's a normal day");
+    console.log("It's a normal day");
+}
